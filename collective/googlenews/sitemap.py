b19754bff5836f060a77b95a191cf0c614b429b9
toutpt <toutpt@gmail.com>
2011-02-17 14:42:19 +0100

    add constraints over the querycatalog: under 1000 news item and published in the last two days.

--- collective/googlenews/sitemap.py
+++ collective/googlenews/sitemap.py
@@ -1,24 +1,29 @@
 from datetime import date
 from zope import component
 from Products.Five import BrowserView
-
+from DateTime import DateTime
 class GoogleNewsSiteMap(BrowserView):
+    """Google News sitemap view generate an xml file you can submit to google
+    news services to have greater refresh news"""
     
     def __init__(self, context, request):
         self.context = context
         self.request = request
         self.state = component.getMultiAdapter((self.context, self.request),name="plone_portal_state")
 
-        
     def news(self):
-        brains = self.context.queryCatalog()
+        constraints = {'sort_limit':1000,
+                      'sort_on':'effective', 'sort_order':'reverse',
+                      'effective': {'query': (DateTime()-2, DateTime()), 'range': 'min:max'}
+                      } #<1000 URLS and published in the last two days.
+        brains = self.context.queryCatalog(**constraints)
         news = [self.brain2news(brain, name=self.state.portal_title) for brain in brains]
         return news
 
     def brain2news(self, brain, name=""):
         #get language and check googlenews language constraints
         language = self.state.language()
-        if len(language)>2 and langague not in ('zh-cn', 'zh-tw'):
+        if len(language)>2 and language not in ('zh-cn', 'zh-tw'):
             language = language[0:2]
         #date format must be AAAA-MM-JJ
         date_str = brain.EffectiveDate
@@ -32,7 +37,3 @@ class GoogleNewsSiteMap(BrowserView):
                 'publication_date':d,
                 'title':brain.Title,
                 'keywords':keywords}
-
-    def default_language(self):
-        return 'fr'
-
