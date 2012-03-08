a93b154bb9e254b2c41485080e58f8e2dcbff5be
hvelarde <hector.velarde@gmail.com>
2012-03-08 18:44:31 -0300

    againg, let's use more standard names

--- collective/googlenews/testing.py
+++ collective/googlenews/testing.py
@@ -1,29 +1,31 @@
-from plone.testing import z2
+# -*- coding: utf-8 -*-
 
 from plone.app.testing import PloneSandboxLayer
 from plone.app.testing import PLONE_FIXTURE
-from plone.app.testing import IntegrationTesting, FunctionalTesting
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import FunctionalTesting
 
-class AddOnLayer(PloneSandboxLayer):
-    default_bases = (PLONE_FIXTURE,)
+
+class Fixture(PloneSandboxLayer):
+
+    defaultBases = (PLONE_FIXTURE,)
 
     def setUpZope(self, app, configurationContext):
         # Load ZCML
         import collective.googlenews
         self.loadZCML(package=collective.googlenews)
 
-        # Install product and call its initialize() function
-        z2.installProduct(app, 'collective.googlenews')
-
     def setUpPloneSite(self, portal):
         # Install into Plone site using portal_setup
         self.applyProfile(portal, 'collective.googlenews:default')
 
-    def tearDownZope(self, app):
-        # Uninstall product
-        z2.uninstallProduct(app, 'collective.googlenews')
 
-FIXTURE = AddOnLayer()
-
-INTEGRATION = IntegrationTesting(bases=(FIXTURE,), name="GoogleNews:Integration")
-FUNCTIONAL = FunctionalTesting(bases=(FIXTURE,), name="GoogleNews:Functional")
+FIXTURE = Fixture()
+INTEGRATION_TESTING = IntegrationTesting(
+    bases=(FIXTURE,),
+    name='collective.googlenews:Integration',
+    )
+FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(FIXTURE,),
+    name='collective.googlenews:Functional',
+    )
