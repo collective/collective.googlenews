08647253e559fd4b94f2ab2ac76fae45a5355ed9
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-01-24 11:10:03 +0100

    Back to development: 1.0rc4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0rc3'
+version = '1.0rc4.dev0'
 long_description = open("README.rst").read() + "\n" + \
                    open(os.path.join("docs", "INSTALL.txt")).read() + "\n" + \
                    open(os.path.join("docs", "CREDITS.txt")).read() + "\n" + \
